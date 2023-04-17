2b005c8ce9fb41288b001e674c3a60f8071e839f
KOKEONHO <rhrjsgh97@gmail.com>
2023-04-17 19:41:33 +0900

    Feat: 기존 RequestHandler.java에서 담당하는 Response 관련 로직을 Response.java 내부에 구현
    기존 RequestHandler.java에서 이뤄지던 Response를 생성하는 로직을 Response.java 내부에 구현

--- src/main/java/response/Response.java
+++ src/main/java/response/Response.java
@@ -4,39 +4,71 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webserver.RequestHandler;
 
-import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Map;
 
 public class Response {
 
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
-    private final DataOutputStream dataOutputStream;
-    private final int lengthOfBodyContent;
+    private final String ACCEPT = "Accept";
+    private final String httpMethod;
+    private final String httpVersion;
+    private final String requestTarget;
     private final String accept;
+    private byte[] body;
+    private String startLine;
+    private String header;
 
-    public Response(DataOutputStream dataOutputStream, int lengthOfBodyContent, String accept) {
-        this.dataOutputStream = dataOutputStream;
-        this.lengthOfBodyContent = lengthOfBodyContent;
-        this.accept = accept;
+    public Response(String httpMethod, String requestTarget, String httpVersion, Map<String, String> requestHeaderMap) throws IOException {
+        this.httpMethod = httpMethod;
+        this.requestTarget = requestTarget;
+        this.httpVersion = httpVersion;
+        this.accept = getAcceptFromHeaderMap(requestHeaderMap);
+        setResponseBody();
+        logger.debug(">> Response -> Response() -> requestTarget: {}", requestTarget);
+        logger.debug(">> Response -> Response() -> accept: {}", accept);
+        logger.debug(">> Response -> Response() -> body: {}", body.toString());
+        setResponseStartLine();
+        setResponseHeader();
     }
 
-    public void send200Header() {
-        try {
-            dataOutputStream.writeBytes("HTTP/1.1 200 OK \r\n");
-            dataOutputStream.writeBytes("Content-Type: " + accept + "; charset=utf-8\r\n");
-            dataOutputStream.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dataOutputStream.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
+    public String getAcceptFromHeaderMap(Map<String, String> requestHeaderMap) {
+        return requestHeaderMap.get(ACCEPT).split(",")[0];
+    }
+
+    public void setResponseStartLine() {
+        StringBuilder startLineBuilder = new StringBuilder();
+        startLineBuilder.append(httpVersion + " " + HttpStatus.OK + " \r\n");
+        startLine = startLineBuilder.toString();
+    }
+
+    public void setResponseBody() throws IOException {
+        if (accept.endsWith("*") || accept.endsWith("css")) {
+            body = Files.readAllBytes(new File("src/main/resources/static/" + requestTarget).toPath());
+            return;
         }
+        body = Files.readAllBytes(new File("src/main/resources/templates/" + requestTarget).toPath());
     }
 
-    public void sendBody(byte[] body) {
-        try {
-            dataOutputStream.write(body, 0, body.length);
-            dataOutputStream.flush();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
+    public void setResponseHeader() {
+        StringBuilder headerBuilder = new StringBuilder();
+        headerBuilder.append("Content-Type: " + accept + ";charset=UTF-8\r\n");
+        headerBuilder.append("Content-Length: " + body.length + "\r\n\r\n");
+        header = headerBuilder.toString();
     }
+
+    public String getStartLine() {
+        return startLine;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public byte[] getBody() {
+        return body;
+    }
+
 }
