ae39ea8a28a413f6daf47f2092c402a9e70e78ba
KOKEONHO <rhrjsgh97@gmail.com>
2023-04-17 19:38:24 +0900

    Refactor: Request.java 내부 생성자를 통한 RequestHandler.java 책임 분담
    RequestHandler.java에 너무 많은 책임이 몰려있던 관계로 Request.java 내부에 생성자를 통한 RequestHandler.java의 책임 분담

--- src/main/java/request/Request.java
+++ src/main/java/request/Request.java
@@ -1,40 +1,70 @@
 package request;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.RequestHandler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Map;
+
 public class Request {
 
     // HTTP 요청을 나타내는 클래스
+    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    private final String requestLine;
+    private StringBuilder requestHeaderBuilder;
+    private Map<String, String> requestHeaderMap;
+    private String httpMethod;
+    private String requestTarget;
+    private String httpVersion;
 
-    private String method;
-    private String uri;
-    private String version;
-
-    public Request(String method, String uri, String version) {      // query가 있는 Request
-        this.method = method;
-        this.uri = uri;
-        this.version = version;
+    public Request(BufferedReader bufferedReader, RequestParser requestParser) throws IOException {     // 생성자
+        requestLine = bufferedReader.readLine();
+        parseRequestLine(requestParser);
+        logger.debug(">> Request -> Request() -> requestLine: {}", requestLine);
+        String headerLine = bufferedReader.readLine();
+//        if (headerLine == null) {
+//            return;
+//        }
+        requestHeaderBuilder = new StringBuilder();
+        while (!headerLine.equals("")) {
+            requestHeaderBuilder.append(headerLine).append("\n");
+            headerLine = bufferedReader.readLine();
+        }
+        logger.debug(">> Request -> httpMethod: {}, requestTarget: {}, httpVersion: {}", httpMethod, requestTarget, httpVersion);
+        parseRequestHeader(requestParser);
     }
 
-    public String getMethod() {
-        return method;
+    public void parseRequestLine(RequestParser requestParser) {
+        String[] parsedRequestLine = requestParser.parseRequestLine(requestLine);
+        logger.debug(">> Request -> parseRequestLine() -> parsedRequestLine: {}", Arrays.toString(parsedRequestLine));
+        httpMethod = parsedRequestLine[0];
+        requestTarget = parsedRequestLine[1];
+        httpVersion = parsedRequestLine[2];
     }
 
-    public void setMethod(String method) {
-        this.method = method;
+    public void parseRequestHeader(RequestParser requestParser) {
+        String requestHeader = requestHeaderBuilder.toString();
+        requestHeaderMap = requestParser.parseRequestHeader(requestHeader);
+        logger.debug(">> Request -> parseRequestHeader() -> requestHeaderMap: {}", requestHeaderMap);
     }
 
-    public String getUri() {
-        return uri;
+    public Map<String, String> getRequestHeaderMap() {
+        return requestHeaderMap;
     }
 
-    public void setUri(String uri) {
-        this.uri = uri;
+    public String getHttpMethod() {
+        return httpMethod;
     }
 
-    public String getVersion() {
-        return version;
+    public String getRequestTarget() {
+        return requestTarget;
     }
 
-    public void setVersion(String version) {
-        this.version = version;
+    public String getHttpVersion() {
+        return httpVersion;
     }
 }
