4f7f45028e6b4e9163ae2a9b4f882765a41343c8
KOKEONHO <rhrjsgh97@gmail.com>
2023-04-17 15:20:35 +0900

    Feat: HTTP 상태 코드를 선언한 HttpStatus.java 생성
    200 OK, 302 Found, 404 Not Found와 같은 HTTP 상태 코드를 선언한 HttpStatus.java 생성

---
+++ src/main/java/HttpStatus.java
@@ -0,0 +1,18 @@
+public enum HttpStatus {
+
+    OK(200, "200 OK"),
+    FOUND(302, "302 Found"),
+    NOT_FOUND(404, "404 Not Found");
+
+    private final int code;
+    private final String message;
+
+    HttpStatus(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getResponseMessage() {
+        return message;
+    }
+}
